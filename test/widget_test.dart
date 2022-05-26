ce3e4c740dd844143a244fe5a606347a81cb65ee
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2022-05-26 19:46:49 +0700

    .

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:gw_gas_ui/main.dart';
+import 'package:qc_ui/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
